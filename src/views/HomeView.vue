8cc696c0a91d46a60aeb1763aa6899f6ae7a84ff
Teto-Novala <tetocoding@gmail.com>
2024-12-05 23:03:37 +0700

    set colors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,7 @@
 <template>
-  <main>Hello World</main>
+  <main>
+    <div class="w-10 h-10 bg-primary"></div>
+    <div class="w-10 h-10 bg-secondary"></div>
+  </main>
 </template>
 <script setup></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FCC822",
+        secondary: "#333",
+      },
+    },
   },
   plugins: [],
 };
